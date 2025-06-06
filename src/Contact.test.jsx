b7ee413686824f237a8c7b3eb25dbd2d86ead919
Justine Gallien <justinegallien@gmail.com>
2025-06-06 18:41:43 -0500

    deleted test file and updated home.jsx

--- src/Contact.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import App from "./App";
-import Contact from "./Components/Contact";
-
-test("renders text on modal", () => {
-  render(<Contact/>);
-  expect(screen.getByText(/Connect With Me/i)).toBeInTheDocument();
-});
--- src/Home.test.jsx
+++ src/Home.test.jsx
@@ -2,8 +2,13 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 import Home from "./Home";
+import { MemoryRouter } from "react-router-dom";
 
 test("renders hello message", () => {
-  render(<Home />);
-  expect(screen.getByText(/Home/i)).toBeInTheDocument();
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+  expect(screen.getByText("I Came. I Saw. I Captured.")).toBeInTheDocument();
 });
